3372093f88baf6b6514e3a3bf5014edc0ecd1af9
Mohinigit <103916913+Mohinigit@users.noreply.github.com>
2022-08-02 02:20:48 -0700

    JS completed

---
+++ JS/Basics/J_1.js
@@ -0,0 +1,6 @@
+console.log("This demonstrates the single line and multi line comment");
+//this is single line comment
+/* This 
+ is 
+ multi line
+ comment */
---
+++ JS/Basics/J_2.js
@@ -0,0 +1,10 @@
+const person = {
+    firstname : "rahul",
+    lastname : "koppula",
+};
+
+console.log(person.firstname);
+console.log(person.lastname)
+
+console.log(person["firstname"]);
+console.log(person["lastname"]);
